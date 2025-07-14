2a2319fa0f4dbb6bdc52251f61644280d3fa3122
Kanav Dahat <kanavdahat55@gmail.com>
2025-07-14 23:21:42 +0530

    Initialised Linked List

---
+++ JavaDataStructures/src/LinkedList/LinkedListUse.java
@@ -0,0 +1,9 @@
+package LinkedList;
+
+public class LinkedListUse {
+	public static void main(String[] args) {
+		Node<Integer> n1 = new Node<Integer>(10);
+		System.out.println(n1.data);
+		System.out.println(n1.next);
+	}
+}
