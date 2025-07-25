0e07b9d51144cf6abbccaca6649c9984bef1fcd9
Kanav Dahat <kanavdahat55@gmail.com>
2025-07-25 22:38:56 +0530

    Added optimized solution for NextNumber

--- JavaDataStructures/src/LinkedList/NextNumber.java
+++ JavaDataStructures/src/LinkedList/NextNumber.java
@@ -1,3 +1,19 @@
+//Problem statement
+//Given a large number represented in the form of a linked list. Write code to increment the number by 1 in-place(i.e. without using extra space).
+//
+//Note: You don't need to print the elements, just update the elements and return the head of updated LL. Input Constraints:
+//1 <= Length of Linked List <=10^6.
+//Detailed explanation ( Input/output format, Notes, Images )
+//Sample Input 1 :
+//3 9 2 5 -1
+//Sample Output 1 :
+//3 9 2 6
+//Sample Input 2 :
+//9 9 9 -1
+//Sample Output 1 :
+//1 0 0 0 
+
+
 package LinkedList;
 
 import java.util.Scanner;
@@ -31,42 +47,74 @@ public class NextNumber {
 		System.out.println(head.data);
 		printRecursive(head.next);
 	}
-	//This is not optimized as this solution will only work for 10 digits only bcoz we've used int
+	public static Node<Integer> reverseLinkedList(Node<Integer> head){
+		if(head==null || head.next==null) {
+			return head;
+		}
+		Node<Integer> reverse = reverseLinkedList(head.next);
+		head.next.next=head;
+		head.next=null;
+		return reverse;
+	}
 	public static Node<Integer> nextLargeNumber(Node<Integer> head) {
 		if(head==null) {
 			return head;
 		}
-		Node<Integer> Temp=head;
-		int count = 0;
-		while(Temp!=null) {
-			int num = count*10;
-			count=num+Temp.data;
-			Temp=Temp.next;
+		head = reverseLinkedList(head);
+		Node<Integer> prev = null;
+		Node<Integer> temp = head;
+		int carry=1;
+		while(temp!=null) {
+			int sum = temp.data+carry;
+			int num = sum%10;
+			carry=sum/10;
+			temp.data=num;
+			prev=temp;
+			temp=temp.next;
 		}
-		count=count+1;
-		Stack<Integer> nextLargeNumber = new Stack<>();
-		while(count>0) {
-			int num = count % 10;
-			count = count/10;
-			nextLargeNumber.add(num);
+		if(carry==1) {
+			Node<Integer> node = new Node<>(carry);
+			prev.next=node;
 		}
-		Node<Integer> newHead = null;
-		Node<Integer> newtail = null;
-		while(!nextLargeNumber.isEmpty()) {
-			int num = nextLargeNumber.pop();
-			Node<Integer> node = new Node<>(num);
-			if(newHead==null) {
-				newHead=node;
-				newtail=node;
-			}else {
-				newtail.next=node;
-				newtail=newtail.next;
-				
-			}
-		}
-		return newHead;
-		
+		head=reverseLinkedList(head);
+		return head;
 	}
+	//This is not optimized as this solution will only work for 10 digits only bcoz we've used int
+//	public static Node<Integer> nextLargeNumber(Node<Integer> head) {
+//		if(head==null) {
+//			return head;
+//		}
+//		Node<Integer> Temp=head;
+//		int count = 0;
+//		while(Temp!=null) {
+//			int num = count*10;
+//			count=num+Temp.data;
+//			Temp=Temp.next;
+//		}
+//		count=count+1;
+//		Stack<Integer> nextLargeNumber = new Stack<>();
+//		while(count>0) {
+//			int num = count % 10;
+//			count = count/10;
+//			nextLargeNumber.add(num);
+//		}
+//		Node<Integer> newHead = null;
+//		Node<Integer> newtail = null;
+//		while(!nextLargeNumber.isEmpty()) {
+//			int num = nextLargeNumber.pop();
+//			Node<Integer> node = new Node<>(num);
+//			if(newHead==null) {
+//				newHead=node;
+//				newtail=node;
+//			}else {
+//				newtail.next=node;
+//				newtail=newtail.next;
+//				
+//			}
+//		}
+//		return newHead;
+//		
+//	}
 	public static void main(String[] args) {
 		Node<Integer> Head = TakeLinkedListInput();
 		Head=nextLargeNumber(Head);
