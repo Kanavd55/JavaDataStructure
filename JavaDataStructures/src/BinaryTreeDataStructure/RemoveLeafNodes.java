e311036b8cc59895a1f1360917aeca1acac5f3ae
Kanav Dahat <kanavdahat55@gmail.com>
2025-07-28 21:18:24 +0530

    Added Binary tree basic practise problem

---
+++ JavaDataStructures/src/BinaryTreeDataStructure/RemoveLeafNodes.java
@@ -0,0 +1,52 @@
+package BinaryTreeDataStructure;
+
+import java.util.Scanner;
+
+public class RemoveLeafNodes {
+	public static BinaryTreeNode<Integer> TakeTreeInputBetter(boolean isRoot,int parentData,boolean isLeft){
+		if(isRoot) {
+			System.out.println("Enter the root data");
+		}
+		else{
+			if(isLeft) {
+				System.out.println("Enter left child of "+ parentData);
+			}else {
+				System.out.println("Enter right child of "+ parentData);
+			}
+		}
+		Scanner s = new Scanner(System.in);
+		int rootData = s.nextInt();
+		if(rootData==-1) {
+			return null;
+		}
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<>(rootData);
+		BinaryTreeNode<Integer> leftChild = TakeTreeInputBetter(false,rootData,true);
+		BinaryTreeNode<Integer> rightChild = TakeTreeInputBetter(false,rootData,false);
+		root.left=leftChild;
+		root.right=rightChild;
+		return root;
+	}
+	public static void PrintBinaryTreeRecursively(BinaryTreeNode<Integer> root) {
+		if(root==null) {
+			return;
+		}
+		System.out.println(root.data);
+		PrintBinaryTreeRecursively(root.left);
+		PrintBinaryTreeRecursively(root.right);
+	}
+	public static BinaryTreeNode<Integer> removeLeafNodes(BinaryTreeNode<Integer> root){
+		if(root==null) {
+			return root;
+		}
+		if(root.left==null && root.right==null) {
+			return null;
+		}
+		removeLeafNodes(root.left);
+		removeLeafNodes(root.right);
+		return root;
+	}
+	public static void main(String[] args) {
+		BinaryTreeNode<Integer> root = TakeTreeInputBetter(true,0,true);
+		PrintBinaryTreeRecursively(root);
+	}
+}
