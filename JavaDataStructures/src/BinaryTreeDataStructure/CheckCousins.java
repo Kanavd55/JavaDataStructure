1bd3ebec2cb2a6ab03b57ac47ee96cf4d4ad55ce
Kanav Dahat <kanavdahat55@gmail.com>
2025-08-11 22:48:48 +0530

    Updated Problem Statement

--- JavaDataStructures/src/BinaryTreeDataStructure/CheckCousins.java
+++ JavaDataStructures/src/BinaryTreeDataStructure/CheckCousins.java
@@ -1,3 +1,18 @@
+//Given the binary Tree and two nodes say ‘p’ and ‘q’. Determine whether the two nodes are cousins of each other or not. Two nodes are said to be cousins of each other if they are at same level of the Binary Tree and have different parents.
+//
+//Do it in O(n).
+//
+//Detailed explanation ( Input/output format, Notes, Images )
+//Constraints :
+//1 <= N <= 10^5
+//
+//Sample Input :
+//5 6 10 2 3 4 -1 -1 -1 -1 9 -1 -1 -1 -1
+//2
+//4
+//Sample Output :
+//true
+
 package BinaryTreeDataStructure;
 
 import java.util.LinkedList;
