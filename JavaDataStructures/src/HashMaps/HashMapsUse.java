641040e5f1307d32d1bfd5e1fa3d2d4d8560d832
Kanav Dahat <kanavdahat55@gmail.com>
2025-08-12 22:14:52 +0530

    Added basics of in-built HashMaps functions

---
+++ JavaDataStructures/src/HashMaps/HashMapsUse.java
@@ -0,0 +1,14 @@
+package HashMaps;
+import java.util.HashMap;
+public class HashMapsUse {
+	public static void main(String[] args) {
+		HashMap<String,Integer> map = new HashMap<>();
+		map.put("abc", 1);
+		map.put("def", 2);
+		if(map.containsKey("abc")) {
+			System.out.println("map has abc");
+		}else {
+			System.out.println("map has def");
+		}
+	}
+}
