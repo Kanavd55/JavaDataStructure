77f2213f5336a85d36e78a849466382b1df85b01
Kanav Dahat <kanavdahat55@gmail.com>
2025-08-17 22:19:07 +0530

    Added In-built Priority Queue basic functions

--- JavaDataStructures/src/PriorityQueue/PriorityQueueUse.java
+++ JavaDataStructures/src/PriorityQueue/PriorityQueueUse.java
@@ -1,7 +1,16 @@
 package PriorityQueue;
-
+import java.util.PriorityQueue;
 public class PriorityQueueUse {
 	public static void main(String[] args) {
-		
+		int[] arr = {4,3,7,6,9,10,12};
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		for(int i=0;i<arr.length;i++) {
+			pq.add(arr[i]);
+		}
+		while(!pq.isEmpty()) {
+			System.out.println(pq.peek());
+			pq.poll();
+		}
+		//By default Priority Queue is Min Priority Queue
 	}
 }
